a8fc8ca70abc3ce8b410d5bafe815804dc48a341
Francisco Santos <francisco-polaco@users.noreply.github.com>
2018-01-11 02:37:35 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.net.InetSocketAddress;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
         server.createContext("/test", new MyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
